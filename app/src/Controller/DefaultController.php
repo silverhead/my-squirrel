891630dfc8779a45b91fdd9d84f994ec93ba0ac1
Nicolas PIN <pin.nicolas@free.fr>
2025-02-01 11:13:24 +0100

    add route for testing

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/default', name: 'default')]
+#[Route('/default', name: 'default_')]
 class DefaultController extends AbstractController
 {
-    
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
